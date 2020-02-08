4a106fbc3f17f0e5b624690e57e41ab2af52c6f6
Max_ <lechasseurmaxime@gmail.com>
2020-02-07 19:08:45 -0500

    Update config.php
    Made some adjustement to comments, removed all FCV reference.

--- config.php
+++ config.php
@@ -7,8 +7,8 @@ $config[totalSrv] = 2;
 # make sure that the ids are set correctly with path and name.
 $config[1] = array(
 	'id' => 1,
-	'btnName' => 'FCV #1',
-	'serverName' => 'Free Candy Van #1',
+	'btnName' => 'My server #1',
+	'serverName' => 'Page hearder name',
 	'cfgTrackerPath' => '../tracker/',
 	'cfgSummaryPath' => '../../../../prbf2/1/json/',
 	'cfgDemoPath' => '../demos/',
@@ -16,8 +16,8 @@ $config[1] = array(
 );
 $config[2] = array(
 	'id' => 2,
-	'btnName' => 'FCV #2',
-	'serverName' => 'Free Candy Van #2',
+	'btnName' => 'My server #2',
+	'serverName' => 'Page hearder name2',
 	'cfgTrackerPath' => '../../2/tracker/',
 	'cfgSummaryPath'=> '../../../../prbf2/2/json/',
 	'cfgDemoPath'=> '../../2/demos/',
@@ -25,16 +25,16 @@ $config[2] = array(
 );
 # Configuration for the community website button.
 $config[site] = array(
-	'URL'=> 'https://forum.bluedrake42.com/index.php?/gc/1-project-reality/',
+	'URL'=> 'My website URL',
 	'btnName'=> 'Forums'
 );
 # Configuration for a voice or direct communication application button.
 $config[voice] = array(
-	'URL'=> 'https://discord.gg/tat4W8R',
+	'URL'=> 'My voice link',
 	'btnName'=> 'Discord'
 );
 # Configuration for the community logo.
-$config[logoURL] = 'https://forum.bluedrake42.com/uploads/monthly_2017_02/freecandyvan.png.1336f08dd08e654753b56847eede037f.png';
+$config[logoURL] = 'My logo URL.png';
 # Configuration for the server time. Background colour is #161414.
 $config[srvTime] =  'http://free.timeanddate.com/clock/i5kdm9ml/n137/fn2/fcfff/tc161414/pcfff161414fff/ahl/tt0/tw1/th1/ta1/tb2';
 ?>
